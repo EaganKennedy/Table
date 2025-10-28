de53d39a067a4d6b2224c405b34c51c3243d6c85
Eagan <eegykennedy@gmail.com>
2025-10-28 14:55:49 -0500

    Basic test framework, constructor and destructor implemented & data() complete.

--- table/table.hpp
+++ table/table.hpp
@@ -3,25 +3,52 @@
 template<typename T>
 class Table{
 public:
-	Table();
-	Table(int r, int c);
-	~Table();
+	Table() : Table(0,0){
+	}
+	Table(int r, int c) {
+		this->r = r;
+		this->c = c;
+		dataptr = new T[r * c];
+	}
+	~Table() {
+		delete[] dataptr;
+	}
 
-	Table(const Table&);
-	Table& operator=(const Table& old);
-	Table(Table&&);
-	Table& operator=(Table&& old)
+	Table(const Table&) {
 
-	T& at(int r, int c) const;
-	T* data() const;
+	}
+	Table& operator=(const Table& old) {
 
-	void resize(int r, int c);
+	}
+	Table(Table&&) {
 
-	Table& operator[](Index i);
-	Table const& operator[](Index i) const;
+	}
+	Table& operator=(Table&& old) {
+
+	}
+
+	T& at(int r, int c) {
+
+	}
+	T const& at(int r, int c) const {
+
+	}
+	T* data() const {
+		return dataptr;
+	}
+	void resize(int r, int c) {
+
+	}
+
+	Table& operator[](int i) {
+
+	}
+	Table const& operator[](int i) const {
+
+	}
 
 private:
-	T* data;
+	T* dataptr;
 	int r;
 	int c;
 };
--- table_test/table_test.cpp
+++ table_test/table_test.cpp
@@ -1,2 +1,37 @@
 ﻿#include "gtest/gtest.h"
 #include "table.hpp"
+
+TEST(Constructor, Init) {
+	ASSERT_NO_THROW(Table<int> x);
+}
+TEST(Constructor, ValueInit) {
+	ASSERT_NO_THROW(Table<int> x(2,2));
+}
+
+TEST(Access, at) {
+
+}
+TEST(Access, thrown) {
+
+}
+TEST(Access, data) {
+
+}
+
+TEST(Copy, init) {
+
+}
+TEST(Copy, assign) {
+
+}
+
+TEST(Move, init) {
+
+}
+TEST(Move, assign) {
+
+}
+
+TEST(Resize, resize) {
+
+}
