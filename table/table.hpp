93c5b1b2403f946f69797666feac947c37b54010
Eagan <eegykennedy@gmail.com>
2025-10-28 13:49:19 -0500

    Basic class framework

--- table/table.hpp
+++ table/table.hpp
@@ -1,1 +1,27 @@
 ﻿#pragma once
+
+template<typename T>
+class Table{
+public:
+	Table();
+	Table(int r, int c);
+	~Table();
+
+	Table(const Table&);
+	Table& operator=(const Table& old);
+	Table(Table&&);
+	Table& operator=(Table&& old)
+
+	T& at(int r, int c) const;
+	T* data() const;
+
+	void resize(int r, int c);
+
+	Table& operator[](Index i);
+	Table const& operator[](Index i) const;
+
+private:
+	T* data;
+	int r;
+	int c;
+};
